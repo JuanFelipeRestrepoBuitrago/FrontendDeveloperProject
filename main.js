fb7beaf77755cc93cd2ce6017b8f0523801f9cff
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2023-07-11 16:41:06 -0500

    Finishing Shopping Cart Section
    The dropdown shopping cart was successfully created and merged into the index or main HTML file

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop_menu');
 
-navbarEmail.addEventListener('click', toggleElement.bind(this, desktopMenu));
-
-function toggleElement(element) {
-    element.classList.toggle('inactive');
-}
-
 const mobileMenu = document.querySelector('.mobile-menu');
 const mobileMenuButton = document.querySelector('.menu-image');
 
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const shoppingCart = document.querySelector('.shopping-cart');
+
+const dropdownElements = [desktopMenu,  mobileMenu,   shoppingCart];
+
+function toggleElement(dropdownElement) {
+    dropdownElements.forEach(element => {
+        if (!element.classList.contains('inactive') && element !== dropdownElement) {
+            element.classList.add('inactive');
+        }
+    });
+    dropdownElement.classList.toggle('inactive');
+}
+
+navbarEmail.addEventListener('click', toggleElement.bind(this, desktopMenu));
+
 mobileMenuButton.addEventListener('click', toggleElement.bind(this, mobileMenu));
+
+shoppingCartIcon.addEventListener('click', toggleElement.bind(this, shoppingCart));
