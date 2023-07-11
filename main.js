3790e145a97f624d00fc09f76bd1ef98d013027d
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2023-07-11 13:05:09 -0500

    Navbar Almost Finished
    The navbar mobile and desktop menus were finished and they work in the page

--- main.js
+++ main.js
@@ -6,3 +6,8 @@ navbarEmail.addEventListener('click', toggleElement.bind(this, desktopMenu));
 function toggleElement(element) {
     element.classList.toggle('inactive');
 }
+
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenuButton = document.querySelector('.menu-image');
+
+mobileMenuButton.addEventListener('click', toggleElement.bind(this, mobileMenu));
